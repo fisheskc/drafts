b9a38c3fb93686fbfea23f2e0087a49fde87036f
Karen Collins <fisheskc@googlemail.com>
2021-06-21 20:51:40 +0100

    I am committing multiple files to place black drafts on my board

--- test.js
+++ test.js
@@ -40,33 +40,37 @@ function buildGameBoard() {
 }
 
 let draftsgrid = {
-  A2: 'c-1',
-  A4: 'c-2',
-  A6: 'c-3',
-  B1: 'c-4',
-  B3: 'c-5',
-  B5: 'c-6',
-  B7: 'c-7',
-  C2: 'c-8',
-  C4: 'c-9',
-  C6: 'c-10'
+  A2: 'circle-1',
+  A4: 'circle-2',
+  A6: 'circle-3',
+  B1: 'circle-4',
+  B3: 'circle-5',
+  B5: 'circle-6',
+  B7: 'circle-7',
+  C2: 'circle-8',
+  C4: 'circle-9',
+  C6: 'circle-10'
 }
 
 // Loop through the original object
 function makeCounter() {
- let ObjCircle = Object.values(draftsgrid)
- let convertStr = ObjCircle.toString()
- let findCircle = convertStr.split("").includes("c")
- console.log(findCircle)
-    if(findCircle==='c') {
-      circle1 = document.createElement("div");
-      draftsgrid[key].classList.add("circle_black");
-      container.appendChild(draftsgrid[key]);
-    }
+  for (const [key, value] of Object.entries(draftsgrid)) {
+    console.log(`${key}: ${value}`);
+    let regCircle = /circle/
+    let findCircle = 
+
+    // if(draftsgrid[key]==='circle-1') {
+    //   draftsgrid[key].classList.add("circle_black");
+    // }
+  }
 }
 console.log(makeCounter());
 
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/;
+let result = extractStr.match(codingRegex);
 
-
-
+for (const value of Object.values(draftsgrid)) {
+    console.log(`${value}`);
+}
 
